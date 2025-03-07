4285af7867ede1cfb36f7d7a3556e0e3232d9328
aliragab11 <ali.ragab@fehris.io>
2025-03-07 15:46:44 +0200

    show genres skeleton

---
+++ src/components/custom/GenreItemSkeleton.tsx
@@ -0,0 +1,12 @@
+import { HStack, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
+
+const GenreItemSkeleton = () => {
+  return (
+    <HStack paddingY={1}>
+      <SkeletonCircle size="10" />
+      <SkeletonText noOfLines={1} h={7} width={'80%'} />
+    </HStack>
+  );
+};
+
+export default GenreItemSkeleton;
--- src/components/custom/GenreList.tsx
+++ src/components/custom/GenreList.tsx
@@ -1,12 +1,15 @@
 import useGenres from '@/hooks/useGenres';
 import getCroppedImage from '@/services/image-url';
 import { HStack, Image, List, Text } from '@chakra-ui/react';
+import GenreItemSkeleton from './GenreItemSkeleton';
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading } = useGenres();
+  const skeletons = Array.from({ length: 12 }, (_, i) => i + 1);
 
   return (
     <List.Root listStyle={'none'}>
+      {isLoading && skeletons.map((el) => <GenreItemSkeleton key={el} />)}
       {genres.map((genre) => (
         <List.Item key={genre.id} paddingY={1}>
           <HStack>
