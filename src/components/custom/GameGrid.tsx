b014d532ceeeec243dff9bd3c6707ff1eff67c99
aliragab11 <ali.ragab@fehris.io>
2025-03-12 20:47:35 +0200

    implement infinite queries

--- src/components/custom/GameGrid.tsx
+++ src/components/custom/GameGrid.tsx
@@ -1,22 +1,43 @@
 import { GameQuery } from '@/App';
-import { SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Button, SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import React from 'react';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data: games, error, isLoading } = useGames(gameQuery);
+  const {
+    data: games,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
   const skeletons = Array.from({ length: 15 }, (_, i) => i + 1);
   if (error) return <Text>{error.message}</Text>;
   return (
-    <SimpleGrid columns={{ sm: 2, md: 2, lg: 3, xl: 4 }} gap={4} p={'10px'}>
-      {isLoading && skeletons.map((el) => <GameCardSkeleton key={el} />)}
-      {games?.results.map((game) => <GameCard key={game.id} game={game} />)}
-    </SimpleGrid>
+    <Box p={'10px'}>
+      <SimpleGrid columns={{ sm: 2, md: 2, lg: 3, xl: 4 }} gap={4}>
+        {isLoading && skeletons.map((el) => <GameCardSkeleton key={el} />)}
+        {games?.pages.map((game, index) => (
+          <React.Fragment key={index}>
+            {game?.results.map((game) => (
+              <GameCard key={game.id} game={game} />
+            ))}
+          </React.Fragment>
+        ))}
+      </SimpleGrid>
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()} my={5}>
+          {isFetchingNextPage ? 'LOADING...' : 'Load more'}
+        </Button>
+      )}
+    </Box>
   );
 };
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 export interface FetchResponse<T> {
   count: number;
   results: T[];
+  next: string | null;
 }
 
 const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
