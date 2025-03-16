5ad8afd87bc2dc110e401fdab32cf9a5aa1551e6
aliragab11 <ali.ragab@fehris.io>
2025-03-16 15:33:45 +0200

    fix the navbar

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -1,12 +1,17 @@
-import { HStack, Image } from '@chakra-ui/react';
+import { Box, HStack, Image } from '@chakra-ui/react';
 import logo from '../../../src/assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router';
 
 const Navbar = () => {
   return (
     <HStack p={'10px'}>
-      <Image src={logo} boxSize={'60px'} />
+      <Link to="/">
+        <Box boxSize={'60px'}>
+          <Image src={logo} alt="logo" objectFit={'cover'} />
+        </Box>
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/custom/SearchInput.tsx
+++ src/components/custom/SearchInput.tsx
@@ -3,13 +3,19 @@ import { InputGroup } from '../ui/input-group';
 import { LuSearch } from 'react-icons/lu';
 import { FormEvent, useRef } from 'react';
 import useGameQueryStore from '@/store';
+import { useNavigate } from 'react-router';
 
 const SearchInput = () => {
+  const navigate = useNavigate();
   const setSearchText = useGameQueryStore((store) => store.setSearchText);
   const refSearch = useRef<HTMLInputElement>(null);
+
   const handleSearch = (event: FormEvent) => {
     event.preventDefault();
-    if (refSearch.current) setSearchText(refSearch.current.value);
+    if (refSearch.current) {
+      setSearchText(refSearch.current.value);
+      navigate('/');
+    }
   };
   return (
     <form style={{ width: '100%' }} onSubmit={handleSearch}>
